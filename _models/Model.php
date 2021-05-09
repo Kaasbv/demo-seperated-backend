d51a596aeda618516b29a51b508a3696250529cd
ExploHash <jbtwaalf@gmail.com>
2021-05-09 20:48:04 +0200

    Added follower model and service

--- _models/Model.php
+++ _models/Model.php
@@ -4,16 +4,8 @@ class Model {
   protected static function fillObject($object, $row){
     foreach ($row as $key => $value) {
       if(!is_null($value)){
-        $object->{self::snakeToCamel($key)} = $value;
+        $object->{$key} = $value;
       }
     }
-  }
-
-  protected static function camelToSnake($input){
-    return preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1_$2', $input);
-  }
-
-  protected static function snakeToCamel($input){
-    return lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ',strtolower($input)))));
   }
 }
