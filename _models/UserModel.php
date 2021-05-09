ac4979d3561966204918afee156ec6502ee138a9
Wasbeer96 <71969409+Wasbeer96@users.noreply.github.com>
2021-05-09 17:13:53 +0200

    Register api call (#3)
    * Register api call
    
    * Fixes, onnodige code weg
    
    * Comments toegevoegd
    
    * Fixes checkifexists
    
    * Removed duplicate line
    
    * Back to protected
    
    * Fixed birthdate
    
    * Fix models
    
    * Fixed small bugs
    
    Co-authored-by: ExploHash <jbtwaalf@gmail.com>

--- _models/UserModel.php
+++ _models/UserModel.php
@@ -10,10 +10,46 @@ class UserModel extends Model {
   public string $firstname;
   public string $middlename;
   public string $lastname;
-  public string $birtdate;
-  protected string $password;
+  public string $birthdate;
+  public string $password;
   public string $date_created;
   public string $date_updated;
+
+  //Check of meegegeven email al in de user tabel bestaat
+  public static function checkIfMailExists($email){
+    $checkquery = "SELECT * FROM User WHERE email = ?";
+    $sql = MysqlHelper::runPreparedQuery($checkquery, [$email], ["s"]);
+    
+    return !empty($sql);
+  }
+  
+  //Check of meegegeven username al in de tabel bestaat
+  public static function checkIfUserExists($username){
+    $checkquery = "SELECT * FROM User WHERE username = ?";
+    $sql = MysqlHelper::runPreparedQuery($checkquery, [$username], ["s"]);
+
+    return !empty($sql);
+  }
+
+  //Insert een nieuwe gebruiker in de user tabel
+  public function create(){
+      $query = "
+          INSERT INTO User
+          (username, email, firstname, middlename, lastname, birthdate, password)
+          VALUES(?, ?, ?, ?, ?, ?, ?);
+      ";
+
+      MysqlHelper::runPreparedQuery($query, [
+          $this->username,
+          $this->email,
+          $this->firstname,
+          $this->middlename,
+          $this->lastname,
+          $this->birthdate,
+          $this->password
+      ], ["s", "s", "s", "s", "s", "s", "s"]);
+
+  }
 
   public static function getByUsername($username) {
     $query = "
--- user/register.php
+++ user/register.php
@@ -1,24 +1,58 @@
 <?php
 //Include de 2 bestanden die wij nodig hebben
-// include_once(__DIR__ . "/../_models/ProductModel.php");
+include_once(__DIR__ . "/../_models/UserModel.php");
 include_once(__DIR__ . "/../_helpers/MysqlHelper.php");
-
 
 //Maak een class aan voor deze api call
 class UserRegister {
+
   public static function run(){
     session_start();
-    //Start een connectie
+        
     MysqlHelper::startConnection();
+    
+    //POST variabelen ophalen
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $birthdate = $_POST['birthdate'];
+    $password = $_POST['password'];
+    $middlename = $_POST["middlename"] ?? "";
 
-    //Doe dingen
-    $data = ["error" => "Jemoeder"];
+    //Check of gebruiker en email bestaat
+    if(userModel::checkIfUserExists($username)){
+      $data = ["execution" => 'failure', 'msg' => 'User already exists'];
+      http_response_code(400);
+      echo json_encode($data);
+    }
+    elseif(userModel::checkIfMailExists($email)){
+      $data = ["execution" => 'failure', 'msg' => 'Mail already exists'];
+      http_response_code(400);
+      echo json_encode($data);
+    }
+    else{
+    //Hash en salt het wachtwoord
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    //Maak usermodel aan
+    $user = new UserModel($firstname, $middlename, $lastname);
+    $user->email = $email;
+    $user->birthdate = $birthdate;
+    $user->password = $password;
+    $user->username = $username;
+
+    //Voer create functie uit, insert op database
+    $user->create();
+    $data = ["execution" => 'success', 'msg' => 'New user created'];
+
     //Geef een response
-    http_response_code(500); //Zet een http code Heel belangrijk!
+    http_response_code(200); //Zet een http code Heel belangrijk!
     echo json_encode($data); // echo de data array in json formaat voor de frontend
-
-    //Sluit de connectie
-    MysqlHelper::closeConnection();
+    
+  }
+  //Sluit de connectie
+  MysqlHelper::closeConnection();
   }
 }
 
