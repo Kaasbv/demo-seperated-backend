3df1a1702b95c65d95c0ea9d5ddba105d84f942a
ExploHash <jbtwaalf@gmail.com>
2021-05-09 22:17:13 +0200

    Added category base for ticket

---
+++ _models/CategoryModel.php
@@ -0,0 +1,16 @@
+<?php
+//Include de bestanden de wij nodig hebben
+include_once(__DIR__ . "/../_models/Model.php");
+include_once(__DIR__ . "/../_helpers/MysqlHelper.php");
+
+class CategoryModel extends Model {
+  
+  public function __construct(
+    public string $name,
+    public string $user_email
+  ){}
+  public string $date_created;
+  public string $date_updated;
+}
+
+?>
