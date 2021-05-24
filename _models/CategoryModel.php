ee9fe3908a8b7327075520d3efaf8c8cbeda877f
Nass1m <70798807+Nass1m@users.noreply.github.com>
2021-05-24 20:19:29 +0200

    rotzooicode

--- _models/CategoryModel.php
+++ _models/CategoryModel.php
@@ -11,6 +11,30 @@ class CategoryModel extends Model {
   ){}
   public string $date_created;
   public string $date_updated;
+
+
+  public static function listByUsername($username){
+    //SQL query om usernames van Category table te halen
+    $category_query = "SELECT * FROM `Category` WHERE `username` = ?";
+
+    $response = MysqlHelper::runPreparedQuery($category_query, [$username], ["s"]);
+    if(empty($response)) return false;
+
+    foreach($response as $data){
+      
+      $object = $data;
+    }
+    
+
+    $object = new CategoryModel($data["username"], $data["name"]);
+    self::fillObject($object, $data);
+    /*foreach($response as $row){
+      $object = new CategoryModel($row['username'], $row['name']);
+      $data[] = $object;
+    }*/
+
+    return $object;
+  }
 }
 
 ?>
