06251c496c245d91c73f8eddf58ff0b90b4395d4
Wasbeer96 <auke.aardoom@gmail.com>
2021-06-13 13:39:44 +0200

    Volgordes fix, response codes

--- user/updatePF.php
+++ user/updatePF.php
@@ -3,36 +3,42 @@
 class UserUpload {
   public static function run(){
     session_start();
+    if(isset($_SESSION["username"])){
 
-    //Haal uit de session de gebruikersnaam van de ingelogde gebruiker
-    $user = $_SESSION["username"];
+      //Haal uit de session de gebruikersnaam van de ingelogde gebruiker
+      $user = $_SESSION["username"];
 
-    //Maak willekeurige string
-    $randomstring = bin2hex(random_bytes(30));
+      //Maak willekeurige string
+      $randomstring = bin2hex(random_bytes(30));
 
-    //Maak locatie voor de file aan
-    $target_dir = "uploads/images/";
-    $target_file = $target_dir . $randomstring . "_profile.gif";
+      //Check het type afbeelding dat geupload is
+      $imagefiletype = $_FILES["fileToUpload"]['type'];
+      $imagesize = $_FILES["fileToUpload"]['size'];
 
-    //Check het type afbeelding dat geupload is
-    $imagefiletype = $_FILES["fileToUpload"]['type'];
+      //Check de grootte van de afbeelding
+      if ($imagesize > 5000000) {
+        http_response_code(400);
+        echo "Sorry, your 'file' is too large ;)";
 
-    //Check de grootte van de afbeelding
-    if ($_FILES["fileToUpload"]["size"] > 5000000) {
-      echo "Sorry, your 'file' is too large ;)";
-      $uploadOk = 0;
-    } elseif($imagefiletype !== "gif" || $imagefiletype !== "png" || $imagefiletype !== "jpeg") {
-      echo "Sorry, you used a trash image format <br/><br/>Please upload a gif, png or jpeg instead :)";
-      $uploadOk = 0;
+      //Check het bestandstype van de afbeelding
+      } elseif($imagefiletype !== "gif" || $imagefiletype !== "png" || $imagefiletype !== "jpeg") {
+        http_response_code(400);
+        echo "Sorry, you used a trash image format <br/><br/>Please upload a gif, png or jpeg instead :)";
+
+      } else {
+      //Maak locatie voor de file aan
+      $target_dir = "uploads/images/";
+      $target_file = $target_dir . $randomstring . "_profile.$imagefiletype";
+
+      //Schrijf de image weg naar schijf
+        $image = $_FILES["fileToUpload"]["tmp_name"];
+        move_uploaded_file($image, $target_file);
+        echo 'Uploaded';
+      }
+
+    //Als de user session niet bestaat, response code 400
     } else {
-      $uploadOk = 1;
-    }
-    
-    //Schrijf de image weg naar schijf
-    if($uploadOk === 1){
-      $image = $_FILES["fileToUpload"]["tmp_name"];
-      move_uploaded_file($image, $target_file);
-      echo 'Uploaded';
+      http_response_code(400);
     }
 
   }
@@ -40,5 +46,3 @@ class UserUpload {
 
 //Run de run functie hiervan
 UserUpload::run();
-
-
