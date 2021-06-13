432aa170a460dfd0d84305e4e4b56e028b9bff5a
Wasbeer96 <71969409+Wasbeer96@users.noreply.github.com>
2021-06-13 17:44:46 +0200

    Dl 55 (#15)
    * Image upload met file check
    
    * Merge branch 'main' of https://github.com/Kaasbv/goalr-backend into DL-29
    
    * Volgordes fix, response codes
    
    * UserModel aangepast en response codes aangepast
    
    * Case when voor file type, imagesize naar max 5MB, random string alleen bij wegschrijven
    
    Co-authored-by: JBeaart <jimmy@levarne.nl>

---
+++ user/updatePF.php
@@ -0,0 +1,74 @@
+<?php
+//Maak de benodigde includes
+include_once(__DIR__ . "/../_models/UserModel.php");
+include_once(__DIR__ . "/../_helpers/MysqlHelper.php");
+
+//Maak een class aan
+class UserUpload {
+  public static function run(){
+    session_start();
+    //Als de user session niet bestaat, response code 401
+    if(!isset($_SESSION["username"])){
+      http_response_code(401);
+      exit;
+    }
+    elseif(!isset($_FILES["fileToUpload"])){
+      http_response_code(406);
+      exit;
+    }
+
+    //Haal uit de session de gebruikersnaam van de ingelogde gebruiker
+    $user = $_SESSION["username"];
+
+    //Variabele voor grootte afbeelding
+    $imagesize = $_FILES["fileToUpload"]['size'];
+    
+    //Bestandstype afbeeldingen bepalen en checken
+    switch(exif_imagetype($_FILES['fileToUpload']['tmp_name'])){
+      case IMAGETYPE_GIF:
+        $imagetype = 'gif';
+        break;
+      case IMAGETYPE_JPEG:
+        $imagetype = 'jpeg';
+        break;
+      case IMAGETYPE_PNG:
+        $imagetype = 'png';
+        break;
+      case IMAGETYPE_WEBP:
+        $imagetype = 'webp';
+        break;
+      default:
+        http_response_code(400);
+        echo "Sorry, you used a trash image format <br/><br/>Please upload a gif, png or jpg instead :)";
+        exit;
+    }
+    
+    //Check het bestandstype van de afbeelding
+    if ($imagesize > 5242880) {
+      http_response_code(400);
+      echo "Sorry, your 'file' is too large ;)"; 
+      exit;
+
+    } else {
+      //Maak locatie voor de file aan met willekeurige string
+      $randomstring = bin2hex(random_bytes(30));
+      $target_dir = "uploads/images/";
+      $target_file = $target_dir . $randomstring . "_profile.$imagetype";
+      
+      //Schrijf de image weg naar schijf
+      $image = $_FILES["fileToUpload"]["tmp_name"];
+      move_uploaded_file($image, $target_file);
+
+      //Start een connectie en zet het pad van de afbeelding in de database
+      MysqlHelper::startConnection();
+      $usermodel = UserModel::getByUserName($user);
+      $usermodel->uploadImage($target_file);
+      MysqlHelper::closeConnection();
+      echo 'Uploaded';
+    }
+
+  }
+}
+
+//Run de run functie hiervan
+UserUpload::run();
